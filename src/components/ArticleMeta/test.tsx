db8fdbb645a3c9fa2c0bda8bf2da2620f2546901
danielVFS <danielv759153@gmail.com>
2021-04-05 15:39:20 -0300

    testing: ArticleMeta

---
+++ src/components/ArticleMeta/test.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { ArticleMeta, ArticleMetaProps } from '.';
+import mock from './mock';
+
+const props: ArticleMetaProps = mock;
+
+describe('<ArticleMeta />', () => {
+  it('should render author and category links', () => {
+    renderTheme(<ArticleMeta {...props} />);
+
+    expect(screen.getByRole('link', { name: 'Daniel Vitor' })).toHaveAttribute(
+      'href',
+      '/author/daniel-vitor',
+    );
+    expect(screen.getByRole('link', { name: 'Tech' })).toHaveAttribute(
+      'href',
+      '/category/tech',
+    );
+    expect(screen.getByRole('link', { name: 'JS' })).toHaveAttribute(
+      'href',
+      '/category/javascript',
+    );
+  });
+
+  it('should format date', () => {
+    renderTheme(<ArticleMeta {...props} />);
+
+    expect(screen.getByText('2021 M03 2')).toHaveAttribute(
+      'datetime',
+      props.createdAt,
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<ArticleMeta {...props} />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
