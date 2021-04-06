c6a2c3887d6a69f81a361fcbdfff9376f11f8ce9
danielVFS <danielv759153@gmail.com>
2021-04-06 20:32:51 -0300

    component: PostCard

---
+++ src/components/PostCard/index.tsx
@@ -0,0 +1,29 @@
+import * as Styled from './styles';
+import Link from 'next/link';
+
+import { PostProps } from '../Post';
+import { Heading } from '../Heading';
+
+export type PostCardProps = PostProps & {
+  slug?: string;
+};
+
+export const PostCard = ({ title, cover, excerpt, slug }: PostCardProps) => {
+  return (
+    <Styled.Wrapper>
+      <Link href={`/post/${slug}`}>
+        <a>
+          <Styled.Cover src={cover.url} />
+        </a>
+      </Link>
+
+      <Heading as="h2" size="small">
+        <Link href={`/post/${slug}`}>
+          <a>{title}</a>
+        </Link>
+      </Heading>
+
+      <Styled.Excerpt>{excerpt}</Styled.Excerpt>
+    </Styled.Wrapper>
+  );
+};
