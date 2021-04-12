4b9517b85d8cb7f71eb2b6da1598d0ac7ead8bcd
danielVFS <danielv759153@gmail.com>
2021-04-12 10:57:06 -0300

    refactor: centralize tags

--- src/templates/PostTemplate/styles.ts
+++ src/templates/PostTemplate/styles.ts
@@ -3,5 +3,9 @@ import styled, { css } from 'styled-components';
 export const TagsContainer = styled.div`
   ${({ theme }) => css`
     padding: 0 ${theme.spacings.large};
+    margin: ${theme.spacings.large} auto;
+    padding: 0 ${theme.spacings.large};
+    max-width: ${theme.sizes.content};
+    width: 100%;
   `}
 `;
